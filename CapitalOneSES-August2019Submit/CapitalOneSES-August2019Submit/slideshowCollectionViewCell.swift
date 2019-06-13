a9eb5351daab4c4efbebb8287b563f8e42ebba8c
Sam Blum <samblum17@icloud.com>
2019-06-13 19:06:21 -0400

    Missed in last commit - Slideshow collection view cell holds image on selected park controller

---
+++ CapitalOneSES-August2019Submit/CapitalOneSES-August2019Submit/slideshowCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  slideshowCollectionViewCell.swift
+//  CapitalOneSES-August2019Submit
+//
+//  Created by Sam Blum on 6/12/19.
+//  Copyright © 2019 Sam Blum. All rights reserved.
+//
+
+import UIKit
+
+/*Cell that holds UIImageView on selected park view controller. Named due to ossible implementation of slideshow feature for photos in future update */
+
+class slideshowCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var myImage: UIImageView!
+    var urlString = ""
+    
+}
